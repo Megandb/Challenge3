399e69b4339e2bab0aee4173b5c32425ec6e564a
Megandb <meggielixdb@gmail.com>
2021-04-07 13:12:57 +0200

    Map doet het
    De map van mapbox doet het nu. Het lag dus aan de height van de div waar hij in zat.
    Toen een klasgenootje ermee kwam dat hij dat probleem ook eerst had vertelde hij ook dat ik moest proberen de height te veranderen. Hierdoor werd de map zichtbaar en heb ik voor niets UREN zitten zoeken naar waarom mijn code voor de map het niet deed.
    
    Hierna heb ik toch wel een 3e box aangemaakt, hier wil ik misschien de sunrise en sunset in zetten. Nu staan er alleen maar mili sec. ik wil de normale tijd hebben, maar dat lukt nog niet.

--- main.js
+++ main.js
@@ -27,6 +27,14 @@ function getAPIdata() {
 		weatherBox.innerHTML = degC + '&#176;C <br>' + response.weather[0].description;
 		
 		document.getElementById("cloudImg").src = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+		//zons 
+		var weatherBox = document.getElementById('sunrise');
+		var sunrise = Math.floor(response.sys.sunrise);
+		weatherBox.innerHTML = 'sunrise :' + sunrise + '<br>';
+
+		var weatherBox = document.getElementById('sunset');
+		var sunset = Math.floor(response.sys.sunrise);
+		weatherBox.innerHTML = 'sunset :' + sunrise + '<br>';
 
 	});
 }
@@ -44,64 +52,13 @@ getAPIdata();
 	var map = new mapboxgl.Map({
 	  container: 'map',
 	  style: 'mapbox://styles/mapbox/streets-v11',
-	  center: [30.2194, 51.2705],
+	  center: [ 4.288788, 52.078663],
 	  style: 'mapbox://styles/mapbox/dark-v10',
 	  pitch: 45,
 	  zoom: 11.15
+
+
 	});
 
-	var geocoder = new MapboxGeocoder({
-	  accessToken: mapboxgl.accessToken,
-	  types: 'country,region,place,postcode,locality,neighborhood',
-	  mapboxgl: mapboxgl
-	});
-
-	// Zoekbalk
-	map.addControl(geocoder, 'top-left');
-
-	// Navigatie knoppen
-	map.addControl(new mapboxgl.NavigationControl());
 
 
-geocoder.on('result', function(response) {
-  document.getElementById('long').innerHTML = response.result.center[0];
-  // long = response.result.center[0];
-  document.getElementById('lat').innerHTML = response.result.center[1];
-  // lat = response.result.center[1];
-  document.getElementById('city').innerHTML = response.result.place_name;
-  // Height();
-  var length = response.result.context.length - 1;
-  console.log(length);
-  land = response.result.context[length].short_code;
-  console.log(land);
-  Country();
-});
-
-function Country(){
-  var rescountries = "https://restcountries.eu/rest/v2/alpha/" + land;
-  fetch(rescountries)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-    document.getElementById('flag').src = response.flag;
-    document.getElementById('people').innerHTML = response.demonym;
-    var languageTotal = "";
-    for(var i = 0; i<response.languages.length ; i++){
-      languageTotal = languageTotal + " " + response.languages[i].name + "<br>"
-    }
-    document.getElementById('languages').innerHTML = languageTotal;
-    var currencyTotal = "";
-    for(var i = 0; i<response.currencies.length ; i++){
-      currencyTotal = currencyTotal + " " + response.currencies[i].name + "<br>"
-    }
-    document.getElementById('currency').innerHTML = currencyTotal;
-    var timezoneTotal = "";
-    for(var i = 0; i<response.timezones.length ; i++){
-      timezoneTotal = timezoneTotal + " " + response.timezones[i] + "<br>"
-    }
-    document.getElementById('timezone').innerHTML = timezoneTotal;
-    document.getElementById('region').innerHTML = response.subregion;
-  })
-}
